8cbeda433fa01c8eee6cf82d64609cf86f56553a
Perelyn-sama <okohebina@gmail.com>
2022-08-25 23:08:36 +0100

    lesson passed

--- exercises/concept/short-fibonacci/src/lib.rs
+++ exercises/concept/short-fibonacci/src/lib.rs
@@ -1,19 +1,21 @@
-/// Create an empty vector
+#![allow(unused)]
+// Create an empty vector
 pub fn create_empty() -> Vec<u8> {
-    unimplemented!()
+    // unimplemented!()
+    vec![0; 0]
 }
-
 /// Create a buffer of `count` zeroes.
 ///
 /// Applications often use buffers when serializing data to send over the network.
 pub fn create_buffer(count: usize) -> Vec<u8> {
-    unimplemented!("create a zeroized buffer of {} bytes", count)
+    // unimplemented!("create a zeroized buffer of {} bytes", count)
+    vec![0; count]
 }
-
 /// Create a vector containing the first five elements of the Fibonacci sequence.
 ///
 /// Fibonacci's sequence is the list of numbers where the next number is a sum of the previous two.
 /// Its first five elements are `1, 1, 2, 3, 5`.
 pub fn fibonacci() -> Vec<u8> {
-    unimplemented!()
+    // unimplemented!()
+    vec![1, 1, 2, 3, 5]
 }
