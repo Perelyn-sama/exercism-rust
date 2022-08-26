06111df6f4d043a8fcb469dd9ec53f473f25e9d7
Perelyn-sama <okohebina@gmail.com>
2022-08-26 15:22:55 +0100

    5 passed 1 fail

--- exercises/concept/magazine-cutout/src/lib.rs
+++ exercises/concept/magazine-cutout/src/lib.rs
@@ -5,5 +5,21 @@
 use std::collections::HashMap;
 
 pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
-    unimplemented!()
+    // unimplemented!()
+    let mut map = HashMap::new();
+    let mut  v: Vec<&str> = vec![];
+
+    for string in magazine {
+        map.insert(string, true);
+    }
+
+
+    for string in note {
+        match map.get(string) {
+            Some(x) => v.push(string),
+            None => println!("You didn't study Web Development")
+        }
+    }
+
+    note == v
 }
