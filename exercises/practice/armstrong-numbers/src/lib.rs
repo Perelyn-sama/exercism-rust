5fb591ed43ee5d28a847394079cb190d602ca05c
Perelyn-sama <okohebina@gmail.com>
2022-09-24 12:18:05 +0100

    armstrong numbers passed

--- exercises/practice/armstrong-numbers/src/lib.rs
+++ exercises/practice/armstrong-numbers/src/lib.rs
@@ -1,3 +1,15 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+    // unimplemented!("true if {} is an armstrong number", num)
+    let mut num_string = String::from(num.to_string());
+    let mut sum: u32 = 0;
+    let len = num_string.len() as u32;
+
+    loop {
+        match num_string.pop() {
+            Some(char) => sum = sum + char.to_digit(16).unwrap().pow(len),
+            None => break
+        }
+    }
+
+    num == sum
 }
