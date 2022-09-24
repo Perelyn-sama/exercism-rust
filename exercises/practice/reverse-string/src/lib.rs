4c155785bba69cceac23b84de264b758a6ea45ab
Perelyn-sama <okohebina@gmail.com>
2022-09-24 05:42:40 +0100

    reverse string passed

--- exercises/practice/reverse-string/src/lib.rs
+++ exercises/practice/reverse-string/src/lib.rs
@@ -1,3 +1,14 @@
 pub fn reverse(input: &str) -> String {
-    unimplemented!("Write a function to reverse {}", input);
+    // unimplemented!("Write a function to reverse {}", input);
+    let mut input = String::from(input);
+    let mut o = String::from("");
+
+    loop {
+        match input.pop() {
+            Some(char) => o.push(char),
+            None => break
+        }
+    }
+
+    o
 }
