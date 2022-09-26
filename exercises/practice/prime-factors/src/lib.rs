bc503f6266be61db9cab41343c64a65f0fe369e3
Perelyn-sama <okohebina@gmail.com>
2022-09-26 23:25:03 +0100

    prime factors passed

--- exercises/practice/prime-factors/src/lib.rs
+++ exercises/practice/prime-factors/src/lib.rs
@@ -1,3 +1,17 @@
-pub fn factors(n: u64) -> Vec<u64> {
-    unimplemented!("This should calculate the prime factors of {}", n)
+pub fn factors(mut n: u64) -> Vec<u64> {
+    // unimplemented!("This should calculate the prime factors of {}", n)
+    // let mut n:
+    let mut divisor: u64 = 2;
+    let mut factors = Vec::new();
+
+    while n != 1 {
+        if n % divisor == 0 {
+            factors.push(divisor);
+            n /= divisor;
+        } else {
+            // divisor = divisor + 1;
+            divisor += 1;
+        }
+    }
+    factors
 }
