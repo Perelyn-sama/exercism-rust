c5c321e646307c10f57cb56df37e9e8b4d822d52
Perelyn-sama <okohebina@gmail.com>
2022-09-27 03:16:40 +0100

    difference of square passed

--- exercises/practice/difference-of-squares/src/lib.rs
+++ exercises/practice/difference-of-squares/src/lib.rs
@@ -1,14 +1,35 @@
-pub fn square_of_sum(n: u32) -> u32 {
-    unimplemented!("square of sum of 1...{}", n)
+pub fn square_of_sum(mut n: u32) -> u32 {
+    let mut sum: u32 = 0;
+    loop {
+        match n {
+            0 => {
+                break;
+            }
+            1.. => {
+                sum += n;
+                n -= 1;
+            }
+        }
+    }
+    sum.pow(2)
 }
 
-pub fn sum_of_squares(n: u32) -> u32 {
-    unimplemented!("sum of squares of 1...{}", n)
+pub fn sum_of_squares(mut n: u32) -> u32 {
+    let mut sum: u32 = 0;
+    loop {
+        match n {
+            0 => {
+                break;
+            }
+            1.. => {
+                sum += n.pow(2);
+                n -= 1;
+            }
+        }
+    }
+    sum
 }
 
 pub fn difference(n: u32) -> u32 {
-    unimplemented!(
-        "difference between square of sum of 1...{n} and sum of squares of 1...{n}",
-        n = n,
-    )
+    square_of_sum(n) - sum_of_squares(n)
 }
